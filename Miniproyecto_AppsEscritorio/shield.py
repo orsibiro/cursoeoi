b3ba56ccb282c7ebe6790ce28c763065c6736c75
orsibiro <biro1orsi@gmail.com>
2021-07-02 11:08:00 +0200

    Subir versión final del miniproyecto

--- Miniproyecto_AppsEscritorio/shield.py
+++ Miniproyecto_AppsEscritorio/shield.py
@@ -8,40 +8,39 @@ from kivy.properties import StringProperty
 
 Builder.load_file('mylayout.kv')
 class MyLayout(Widget):
+    out_text = StringProperty("")
 
     def cambio_texto(self, widget):
-        s = widget.text
         print(widget.text)
-
+        s = str(widget.text)
 
     def show_slider(self, widget):
-        clave = widget.value
         print(widget.value)
+        clave = int(widget.value)
 
-    def cifra(s, clave=3):
+    def cifra(self):
         buff = []
-        for c in s:
-            num = ord(c)
-            if 65 <= num < 91:
-                new_num = ((num - 65 + clave) % 26) + 65
-                buff.append(str(chr(new_num)))
-            elif 97 <= num < 123:
-                new_num = ((num - 97 + clave) % 26) + 97
-                buff.append(str(chr(new_num)))
-            else:
-                buff.append(c)
-        return ''.join(buff)
-
+        if int(self.clave.value) != 0:
+            for c in self.s.text:
+                num = ord(c)
+                if 65 <= num < 91:
+                    new_num = ((num - 65 + int(self.clave.value)) % 26) + 65
+                    buff.append(str(chr(new_num)))
+                elif 97 <= num < 123:
+                    new_num = ((num - 97 + int(self.clave.value)) % 26) + 97
+                    buff.append(str(chr(new_num)))
+                else:
+                    buff.append(c)
+            text_output_text = ''.join(buff)
+            self.out_text = f"{text_output_text}"
+        else:
+            text_output_text = "¡Cuidado, has seleccionado 0 como clave y tu mensaje no se cifrará!"
+            self.out_text = f"{text_output_text}"
+        return print(text_output_text)
 
 class ShieldApp(App):
     def build(self):
         return MyLayout()
-    
-
-
-# def main():
-#     app = ShieldApp() 
-#     app.run()
 
 
 if __name__ == '__main__':
